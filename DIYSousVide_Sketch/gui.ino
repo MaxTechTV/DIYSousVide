beceb19384fa5f9663579f7af63a50a128544aa0
Max H <max@maxtechtv>
2016-10-08 19:44:51 +0200

    First working prototype
    Yay - its alive. First working prototype !

--- DIYSousVide_Sketch/gui.ino
+++ DIYSousVide_Sketch/gui.ino
@@ -2,38 +2,42 @@ void initLCD() {
   lcd.begin();
   lcd.backlight();
   lcd.setCursor(0, 0);
-  lcd.print("INIT SCREEN");
+  lcd.print("DIYSousVide v1.1");
+  lcd.setCursor(0,1);
+  lcd.print("by MaxTechTV");
+ 
+  delay(1000);
 }
 
 void showTemp(int t) {
-  lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print("Temperatur: ");
+  lcd.print("Temp:      ");
+  lcd.setCursor(6, 0);
   lcd.print(t);
-  lcd.print(" °C");
+  lcd.print(" C");
 }
 
-void showTime(int tM, int tH){
-  lcd.clear();
+void showTime(int tH,int tM){
   lcd.setCursor(0, 0);
-  lcd.print("Zeit: ");
+  lcd.print("Zeit:          ");
+  lcd.setCursor(6, 0);
   lcd.print(tH);
-  lcd.print(" Hours ");
+  lcd.print(" h ");
   lcd.print(tM);
-  lcd.print(" Minutes");
+  lcd.print(" min");
 }
 void showCurrentTime(){
 
-  long cT = startTime-millis();
+  long cT = millis()-startTime;
 
   int tH = (int)(((cT/1000)/60)/60);
   int tM = (int)(((cT/1000)/60)%60);
-  lcd.clear();
   lcd.setCursor(0, 1);
-  lcd.print("Zeit: ");
-  lcd.print(tH);
-  lcd.print(" Hours ");
-  lcd.print(tM);
-  lcd.print(" Minutes");
+  lcd.print("Zeit:          ");
+  lcd.setCursor(6, 1);
+  lcd.print(targetHours-tH);
+  lcd.print("h ");
+  lcd.print(targetMinutes-tM);
+  lcd.print("min");
 
   }
